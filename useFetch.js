c9c48bcf4ebfb89fa5ab87914fb8815fb9917332
Luis C <37582708+lcarbajo-xq@users.noreply.github.com>
2020-11-20 21:53:17 +0100

    Otro commit

---
+++ useFetch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from "react";
+
+export const useFetch = (url = "") => {
+  const isMounted = useRef(true);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setState({ ...state, loading: true });
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (isMounted.current) {
+          setState({
+            loading: false,
+            error: null,
+            data,
+          });
+        }
+      })
+      .catch(() => {
+        setState({
+          data: null,
+          loading: false,
+          error: "No se encontro la pagina",
+        });
+      });
+  }, [url]);
+  return state;
+};
